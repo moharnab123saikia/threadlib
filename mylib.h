cc1259e17ccfe9b5a34313aa48953148223b7b9b
Moharnab Saikia <moharnab123saikia@gmail.com>
2016-02-03 19:06:16 -0500

    added semaphore code

---
+++ mylib.h
@@ -0,0 +1,20 @@
+#ifndef MYTHREADLIB_H
+#define MYTHREADLIB_H
+#include <ucontext.h>
+#include "queue.h"
+#define STACK 16384 //16KB stack size
+
+//Structure for thread
+typedef struct _MyThread{
+	struct _MyThread *parent;
+	struct Queue *children;
+	struct _MyThread *blocked;
+	ucontext_t *this_context;
+}_MyThread;
+
+typedef struct _MySemaphore{
+	int value;
+	struct Queue *sem_queue;
+}_MySemaphore;
+
+#endif //MYTHREADLIB_H
